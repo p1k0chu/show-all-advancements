f8f3df924b0a84c4630f77d58e69b7bff928138c
p1k0chu <anatoliyid0207@gmail.com>
2024-11-21 17:50:53 +0200

    update to 1.19.4

--- src/main/java/com/github/p1k0chu/mcmod/show_all_advancements/mixin/PlayerAdvancementsMixin.java
+++ src/main/java/com/github/p1k0chu/mcmod/show_all_advancements/mixin/PlayerAdvancementsMixin.java
@@ -2,35 +2,52 @@ package com.github.p1k0chu.mcmod.show_all_advancements.mixin;
 
 import com.github.p1k0chu.mcmod.show_all_advancements.IServerAdvancementManager;
 import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementProgress;
 import net.minecraft.advancements.DisplayInfo;
 import net.minecraft.server.PlayerAdvancements;
 import net.minecraft.server.ServerAdvancementManager;
+import net.minecraft.server.advancements.AdvancementVisibilityEvaluator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.function.Predicate;
 
 
 @Mixin(PlayerAdvancements.class)
 public abstract class PlayerAdvancementsMixin {
+    @Shadow
+    protected abstract void markForVisibilityUpdate(Advancement advancement);
 
-    @Shadow protected abstract void ensureVisibility(Advancement advancement);
+    @Shadow
+    public abstract AdvancementProgress getOrStartProgress(Advancement advancement);
 
-    @Inject(method = "shouldBeVisible", at = @At("HEAD"), cancellable = true)
-    private void shouldBeVisible(Advancement advancement, CallbackInfoReturnable<Boolean> cir) {
-        DisplayInfo display = advancement.getDisplay();
+    @Redirect(method = "updateTreeVisibility", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/advancements/AdvancementVisibilityEvaluator;evaluateVisibility(Lnet/minecraft/advancements/Advancement;Ljava/util/function/Predicate;Lnet/minecraft/server/advancements/AdvancementVisibilityEvaluator$Output;)V"))
+    private void updateTreeVisibility(Advancement advancement, Predicate<Advancement> predicate, AdvancementVisibilityEvaluator.Output output) {
+        Advancement root = advancement.getRoot();
+        updateTreeRecursive(root, output);
+    }
 
-        if(display != null && !display.isHidden()) {
-            cir.setReturnValue(true);
+    @Unique
+    private void updateTreeRecursive(Advancement advancement, AdvancementVisibilityEvaluator.Output output) {
+        Iterable<Advancement> children = advancement.getChildren();
+
+        for (Advancement child : children) {
+            updateTreeRecursive(child, output);
         }
+
+        DisplayInfo displayInfo = advancement.getDisplay();
+        output.accept(advancement, displayInfo == null || (!displayInfo.isHidden() || getOrStartProgress(advancement).isDone()));
     }
 
     @Inject(method = "load", at = @At("RETURN"))
     private void ensureRootsVisible(ServerAdvancementManager serverAdvancementManager, CallbackInfo ci) {
         Iterable<Advancement> roots = ((IServerAdvancementManager) serverAdvancementManager).show_all_advancements$getRoots();
 
-        roots.forEach(this::ensureVisibility);
+        roots.forEach(this::markForVisibilityUpdate);
     }
 }
